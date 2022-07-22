7286c68c16b39f2cd77aebce356977fad429dd1b
Jo-Ros <94191307+Jo-Ros@users.noreply.github.com>
2022-07-22 11:00:38 +0200

    fix: like: add debounceTime & like Array length

--- front-end/src/app/post/components/post-details/post-details.component.ts
+++ front-end/src/app/post/components/post-details/post-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject, debounce, Observable, Subject, tap, timer } from 'rxjs';
+import { BehaviorSubject, debounce, debounceTime, interval, Observable, Subject, take, tap } from 'rxjs';
 
 import { AuthService } from 'src/app/auth/auth.service';
 import { Post } from '../../models/post.model';
@@ -15,8 +15,9 @@ export class PostDetailsComponent implements OnInit {
 
   buttonText!: string;
   userId!: string;
-  //numberOfLikes$!: Subject<number>;
-  //liked$ = new BehaviorSubject<boolean>(false);
+  numberOfLikes!: number;
+  likeClicked$ = new Subject<string>();
+
   post$!: Observable<Post>;
   isAuthor$ = new BehaviorSubject<boolean>(false);
 
@@ -31,41 +32,59 @@ export class PostDetailsComponent implements OnInit {
     const postId = this.route.snapshot.params['id'];
     this.post$ = this.postService.getPostById(postId).pipe(
       tap(post => {
-        // this.numberOfLikes$ = post.usersIdLiked.length
+        this.numberOfLikes = post.usersIdLiked.length;
+
         if (post.userId === this.userId) {
           this.isAuthor$.next(true);
         }
         if (post.usersIdLiked.find(user => user === this.userId)) {
-          //this.liked$.next(true);
           this.buttonText = 'Unlike';
         } 
         else {
           this.buttonText = 'Like';
         }
       })
-    )
+    );
+
+    const likeClickedDebounced = this.likeClicked$.pipe( debounceTime(500));
+
+    likeClickedDebounced.subscribe((id: string) => {
+      this.onLike(id)
+    })
   }
   
-  onLike(id: string) {
+  //====
+  onClickLike(id: string) {
+    this.likeClicked$.next(id)
+  }
 
+  onLike(id: string) {
     if (this.buttonText === 'Like') {
       this.postService.likePost(id, this.userId).pipe(
-        tap(() => { this.buttonText = 'Unlike' }),
+        tap(() => { 
+          this.buttonText = 'Unlike'
+          this.numberOfLikes +=1;
+        }),
       ).subscribe();
     }
     else {
       this.postService.likePost(id, this.userId).pipe(
-        tap(() => { this.buttonText = 'Like' })
+        tap(() => { 
+          this.buttonText = 'Like',
+          this.numberOfLikes -=1;
+         })
       ).subscribe()
     }
   }
 
+  //===
   onDelete(id: string) {   
       this.postService.deletePost(id).pipe(
         tap(() => this.router.navigate(['/']))
       ).subscribe()
   }
 
+  //===
   onEdit(id: string) {
     this.router.navigate([`${id}/modify`])
   }
